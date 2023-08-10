d1a56d04b66e5c585620da26ff815879d2968f1f
sax-yusuph <yusuphshamsondeen@gmail.com>
2023-08-10 09:27:48 +0100

    remove fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/header";
 import { Providers } from "@/components/providers";
-import { fontMono, fontSans } from "@/lib/fonts";
-import { cn } from "@/lib/utils";
+
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
@@ -14,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn("font-sans antialiased", fontSans.variable, fontMono.variable)}>
+      <body>
         <Toaster />
         <Providers attribute="class" defaultTheme="system" enableSystem>
           <div className="flex min-h-screen flex-col">
