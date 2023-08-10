4fdf5a2f84ec496fcca450c04de3994b58bb6dff
sax-yusuph <yusuphshamsondeen@gmail.com>
2023-08-10 09:57:46 +0100

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@/components/header";
 import { Providers } from "@/components/providers";
 
+import { fontMono, fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
@@ -13,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body>
+      <body className={cn("font-sans antialiased", fontSans.variable, fontMono.variable)}>
         <Toaster />
         <Providers attribute="class" defaultTheme="system" enableSystem>
           <div className="flex min-h-screen flex-col">
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
