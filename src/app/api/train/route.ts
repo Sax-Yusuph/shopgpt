42fd5b81c3e1386e67d09c474e12590a7fb2f56f
sax-yusuph <yusuphshamsondeen@gmail.com>
2023-08-10 08:57:53 +0100

    refactor

--- src/app/api/train/route.ts
+++ src/app/api/train/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: NextRequest) {
   return NextResponse.json({ status: "success" });
 }
 
-export function transform(data: Product) {
+function transform(data: Product) {
   let content = "";
 
   for (const d in data) {
