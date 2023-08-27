59ababb1b899175480e73510978abd4886bc1c30
sax-yusuph <yusuphshamsondeen@gmail.com>
2023-08-27 10:46:41 +0100

    add next chunk

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSdk } from "../sdk";
 
+import { ChatCompletionRequestMessageRoleEnum } from "openai-edge";
 import { COMPLETION_MODEL } from "../utils/constants";
+import { getContextMessages } from "../utils/formatter";
 
 export const runtime = "edge";
 export async function POST(request: NextRequest) {
@@ -10,6 +13,18 @@ export async function POST(request: NextRequest) {
   if (!messages) {
     return handleError("Missing text parameter");
   }
+
+  const contextMessages = getContextMessages(messages, preferredStore);
+  const { openAi } = getServerSdk();
+
+  const [userMessage] = contextMessages
+    .filter(({ role }) => role === ChatCompletionRequestMessageRoleEnum.User)
+    .slice(-1);
+
+  if (!userMessage) {
+    return handleError("No message with role 'user'");
+  }
+
   return NextResponse.json("WIP");
 }
 
