762274f2c0a81cfc6c843459bd57825d185d3133
sax-yusuph <yusuphshamsondeen@gmail.com>
2024-01-16 10:52:38 +0100

    refactor

--- chat-app/README.md
+++
@@ -1,1 +0,0 @@
-![image](https://github.com/Sax-Yusuph/sinapis-shopper/assets/57273445/1423268b-5b5c-4e84-b3ff-66977dab7e91)
--- chat-app/src/app/store.ts
+++
@@ -1,8 +0,0 @@
-import { proxy } from "valtio";
-
-export const chatState = proxy({ loading: false });
-export type ChatState = { loading: boolean };
-
-export const updateState = (state: boolean) => {
-  chatState.loading = state;
-};
